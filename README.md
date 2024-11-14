c4c0fe4b75810ca76b49805ba4b59d5cfaa5337e
Shinya-Takiyoshi <garo.savior@gmail.com>
2024-11-14 21:18:38 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rabbitMQ
 rabiitMQ動作確認
+
+prepare
+
+```docker run -d --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management```
---
+++ src/main/java/org/example/Send.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+public class Send {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");  // ローカルのRabbitMQに接続
+        factory.setPort(5672);
+
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "test";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
